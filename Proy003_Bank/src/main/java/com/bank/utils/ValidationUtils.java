027a63316dd56c4fa07cc78ae60064474430edf2
rcatalina <111575571+rcatalina@users.noreply.github.com>
2022-10-07 11:07:30 +0200

    dia 5 del curso
    wip changes, tres en raya

---
+++ Proy003_Bank/src/main/java/com/bank/utils/ValidationUtils.java
@@ -0,0 +1,13 @@
+package com.bank.utils;
+
+public abstract class ValidationUtils {
+
+	public static boolean validateNotNullObjects(Object... args) {
+
+		boolean isOK = true;
+
+		return isOK;
+
+	}
+
+}
