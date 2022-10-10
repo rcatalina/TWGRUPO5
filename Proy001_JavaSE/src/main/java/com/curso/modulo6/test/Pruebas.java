aacc588c55f7235074ee79be39eaed65ed094e45
rcatalina <111575571+rcatalina@users.noreply.github.com>
2022-10-10 14:22:39 +0200

    semana 2 lunes
    wip changes polimorfismo

--- Proy001_JavaSE/src/main/java/com/curso/modulo6/test/Pruebas.java
+++ Proy001_JavaSE/src/main/java/com/curso/modulo6/test/Pruebas.java
@@ -1,18 +1,23 @@
 package com.curso.modulo6.test;
 
+import java.util.UUID;
+
 import com.curso.modulo6.domain.Director;
 import com.curso.modulo6.domain.Empleado;
 import com.curso.modulo6.domain.Gerente;
 import com.curso.modulo6.domain.Ingeniero;
+import com.curso.modulo6.domain.Secretario;
 
 public class Pruebas {
 
 	public static void main(String[] args) {
 
+		// EMPLEADO
 		Empleado e1 = new Empleado("Ruben", "Catalina");
 		e1.setSalary(Double.valueOf(50000));
 		e1.mostrarDetalle();
 
+		// GERENTE
 		Gerente g1 = new Gerente("Manuel", "García");
 		g1.setSalary(Double.valueOf(70000));
 		g1.setDepartamento("Administración");
@@ -22,10 +27,12 @@ public class Pruebas {
 		g2.setSalary(Double.valueOf(60000));
 		g2.mostrarDetalle();
 
+		// INGENIERO
 		Empleado i1 = new Ingeniero("Pepe", "Lopez");
 		i1.setSalary(Double.valueOf(40000));
 		i1.mostrarDetalle();
 
+		// DIRECTOR
 		Empleado d1 = new Director("Pepe", "Lopez");
 
 		try {
@@ -37,6 +44,11 @@ public class Pruebas {
 		d1.setSalary(Double.valueOf(47000));
 		((Director) d1).setDepartamento("Dirección");
 		d1.mostrarDetalle();
+
+		// SECRETARIO
+		Empleado s1 = new Secretario("Irantzu", "Gonzalez");
+		((Secretario) s1).atenderLlamada();
+		((Secretario) s1).setNumSegSocial(UUID.randomUUID().toString());
 
 	}
 
