9817d1ed9c682d6df7aaf88fa782be859ee417e2
rcatalina <111575571+rcatalina@users.noreply.github.com>
2022-10-11 11:08:44 +0200

    semana 2 martes
    OJO! classpath/project/prefs changes: VSCODE migration?

--- Proy001_JavaSE/src/main/java/com/curso/modulo6/domain/Director.java
+++ Proy001_JavaSE/src/main/java/com/curso/modulo6/domain/Director.java
@@ -2,30 +2,30 @@ package com.curso.modulo6.domain;
 
 public class Director extends Gerente {
 
-	private Double comision;
+    private Double comision;
 
-	public Double getComision() {
-		return comision;
-	}
+    public Double getComision() {
+        return comision;
+    }
 
-	public Director(String name, String surname) {
-		super(name, surname);
-		this.comision = Double.valueOf(0);
-	}
+    public Director(String name, String surname) {
+        super(name, surname);
+        this.comision = Double.valueOf(0);
+    }
 
-	public void aumentarComision(Double inc) throws Exception {
-		if (inc != null)
-			this.comision += inc;
-		else
-			throw new Exception("El aumento de comision no puede ser NULL.");
-	}
+    public void aumentarComision(Double inc) throws Exception {
+        if (inc != null)
+            this.comision += inc;
+        else
+            throw new Exception("El aumento de comision no puede ser NULL.");
+    }
 
-	@Override
-	public void mostrarDetalle() {
-		super.mostrarDetalle();
+    @Override
+    public void mostrarDetalle() {
+        super.mostrarDetalle();
 
-		if (this.comision != null)
-			System.out.println("COMISION: " + this.comision);
-	}
+        if (this.comision != null)
+            System.out.println("COMISION: " + this.comision);
+    }
 
 }
--- Proy001_JavaSE/src/main/java/com/curso/modulo6/domain/Empleado.java
+++ Proy001_JavaSE/src/main/java/com/curso/modulo6/domain/Empleado.java
@@ -2,50 +2,57 @@ package com.curso.modulo6.domain;
 
 public class Empleado {
 
-	private String name;
-	private String surname;
-	private Double salary;
+    private static final Double SALARIO_BASE = Double.valueOf(12000);
 
-	public Empleado(String name, String surname) {
-		super();
-		this.name = name;
-		this.surname = surname;
-	}
+    private String name;
+    private String surname;
+    private Double salary;
 
-	public String getName() {
-		return name;
-	}
+    public Empleado(String name, String surname) {
+        super();
+        this.name = name;
+        this.surname = surname;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public static Double getSalarioBase() {
+        return SALARIO_BASE;
+    }
 
-	public String getSurname() {
-		return surname;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Double getSalary() {
-		return salary;
-	}
+    public String getSurname() {
+        return surname;
+    }
 
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
-	public void mostrarDetalle() {
-		System.out.println("\nEMPLEADO");
-		System.out.println("TIPO: " + this.getClass().getSimpleName());
-		System.out.println("Nombre: " + this.name);
-		System.out.println("Apellido: " + this.surname);
-		System.out.println("Salario: " + this.salary);
-	}
+    public Double getSalary() {
+        return salary;
+    }
 
-	public static void saludar() {
-		System.out.println("Hola!");
-	}
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void mostrarDetalle() {
+        System.out.println("\nEMPLEADO");
+        System.out.println("TIPO: " + this.getClass().getSimpleName());
+        System.out.println("Nombre: " + this.name);
+        System.out.println("Apellido: " + this.surname);
+        System.out.println("Salario: " + this.salary);
+    }
+
+    public static void saludar() {
+        System.out.println("Hola!");
+
+    }
 
 }
