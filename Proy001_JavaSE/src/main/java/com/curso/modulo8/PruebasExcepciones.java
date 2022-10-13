dd4d0254366e799703c41cf2bdfef6200f27ab40
rcatalina <111575571+rcatalina@users.noreply.github.com>
2022-10-13 13:35:42 +0200

    semana 2 jueves
    excepciones

---
+++ Proy001_JavaSE/src/main/java/com/curso/modulo8/PruebasExcepciones.java
@@ -0,0 +1,25 @@
+package com.curso.modulo8;
+
+public class PruebasExcepciones {
+
+    private PruebasExcepciones() {
+
+    }
+
+    public static void main(String[] args) {
+
+        int result = doble("22");
+
+        System.out.println("El doble de 22 es " + result + ".");
+
+    }
+
+    public static int doble(String num) {
+
+        int numeroInicial = Integer.parseInt(num);
+
+        return numeroInicial * 2;
+
+    }
+
+}
