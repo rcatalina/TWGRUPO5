90ee4a5f79e05135e33ac1d74fc3abb710f95a00
rcatalina <111575571+rcatalina@users.noreply.github.com>
2022-10-13 13:54:41 +0200

    semana 2 jueves
    wip changes

--- Proy001_JavaSE/src/main/java/com/curso/modulo8/PruebasExcepciones.java
+++ Proy001_JavaSE/src/main/java/com/curso/modulo8/PruebasExcepciones.java
@@ -7,17 +7,13 @@ public class PruebasExcepciones {
     }
 
     public static void main(String[] args) {
-
         int result = doble("22");
-
         System.out.println("El doble de 22 es " + result + ".");
 
     }
 
     public static int doble(String num) {
-
         int numeroInicial = Integer.parseInt(num);
-
         return numeroInicial * 2;
 
     }
